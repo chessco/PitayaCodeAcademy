1bfd8e31c530cd8e142347e06c5a52a3a67e9356
chessco <chesssco@gmail.com>
2026-02-18 07:40:31 -0700

    chore: ignore sensitive and temporary files

--- api/gen_hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-const password = 'pitaya123';
-bcrypt.hash(password, 10).then(hash => {
-    console.log('HASH:', hash);
-    bcrypt.compare(password, hash).then(match => {
-        console.log('VERIFIED:', match);
-    });
-});
